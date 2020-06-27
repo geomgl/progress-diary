ba658cad6662264b54fb0baf757db7c28d29976d
George Magloire <gmagloire22@gmail.com>
2020-06-26 21:14:07 -0400

    added database fie. Temporarily using python list as in-memory database for journal entries

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
+from database import add_entry, view_entries
+
 menu = """Please select one of the following of the follwoing options:
 1) New entry
 2) View Entries
 3) Exit
 
-Your selection """
+Your selection: """
 
 welcome = "Welcome to programming progress diary!"
 
 user_input = input(menu)
 while user_input != 3:
-    if user_input == 1:
-        print("Adding...")
-    elif user_input == 2:
-        print("Viewing...")
+    if user_input == "1":
+        entry_content = input("What did you learn today?")
+        entry_date = input("Enter date: ")
+        add_entry(entry_content, entry_date)
+    elif user_input == "2":
+        entries = view_entries()
+        for entry in entries:
+            print(f"{entry['date']}\n{entry['content']}\n\n")
     else:
         print("Please enter either 1, 2, or 3")
     
